48a9ef34db5dc75bc7215e065a8268c743258dd1
Janelle <janellezucc@gmail.com>
2020-01-09 13:03:11 -0500

    added projects

--- assets/js/typeit/typeit.js
+++ assets/js/typeit/typeit.js
@@ -1,8 +1,7 @@
-$('.words-effect').typeIt({
-    speed: 50,
-    startDelay: 900
-  })
-  .type('The most versatile animated typing utility on the internte')
+new TypeIt('.words-effect', {
+  speed: 50,
+  startDelay: 900
+}).type('The most versatile animated typing utility on the internte')
   .pause(300)
   .delete(2)
   .pause(250)
@@ -19,3 +18,34 @@ $('.words-effect').typeIt({
     speed: 50,
     waitUntilVisible: true
   }).go();*/
+
+
+// var words = ['abcd', 'efgh'];
+// var container = document.querySelector('.words-effect');
+
+// function TypeWriter(words, container) {
+//   this.currentWordIndex = 0;
+//   this.words = words;
+//   this.container = container;
+//   this.start();
+// }
+
+// TypeWriter.prototype.next = function() {
+//   this.currentWordIndex = (this.currentWordIndex + 1) < this.words.length ? (this.currentWordIndex + 1) : 0;
+//   this.type();
+// }
+
+// TypeWriter.prototype.start = function () {
+//   console.log(this.words, this.container);
+//   var word = this.words[this.currentWordIndex];
+//   var typeLength = (word.length * 2);
+
+
+  
+// }
+
+// TypeWriter.prototype.type = function(word, index) {
+
+// }
+
+// var typeWriter = new TypeWriter(words, container);
